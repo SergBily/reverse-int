dc535d6f34b7b9a1ef77f7ab3f22ee08abdb5450
Sergbily <SergBily.dev@gmail.com>
2022-01-14 12:57:37 +0300

    feat: add solution function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let number = n.toString().split('')
+	if (number[0] == '-') {
+		number = Math.abs(number.join(''))
+		return number.toString().split('').reverse().join('')
+	} else {
+		return n.toString().split('').reverse().join('')
+	}
 }
